633868230f23f5c7293a5e91d10381893db1b7db
Dylan LUCHMUN <dylan.luchmun@epitech.eu>
2020-01-06 15:13:34 +0100

    first start project weather and interface mobile without code

---
+++ Weather/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Dylan Luchmun on 06/01/2020.
+//  Copyright © 2020 Dylan Luchmun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var backgroundView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
